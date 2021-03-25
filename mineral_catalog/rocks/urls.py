c1025cff03b15c0e36c1b7fe386243760c18697d
ziru13 <ziru.fishu@gmail.com>
2021-03-25 11:44:04 +0800

    add load_rocks view

--- mineral_catalog/rocks/urls.py
+++ mineral_catalog/rocks/urls.py
@@ -6,6 +6,7 @@ app_name = 'courses'
 # make our variable, a list, in the old Django, it didn't use to be a list, now it's just a list
 urlpatterns = [
     path('', views.rock_list, name='rock_list'),
+    path('load-rocks/', views.load_rocks, name='load_rocks'),
     # path('<int:course_pk>/<int:step_pk>/', views.step_detail, name='step_detail'),
     # path('<int:pk>/', views.course_detail, name='course_detail'),
 ]
--- mineral_catalog/rocks/views.py
+++ mineral_catalog/rocks/views.py
@@ -1,8 +1,22 @@
+import json
+
 from django.shortcuts import render
 
 # Create your views here.
 # from django.shortcuts import HttpResponse
 from .models import Rock
+
+
+def load_rocks(request):
+    json_file = 'minerals.json'
+    db_file = 'minerals.db'
+
+    # open json
+    with open(json_file, encoding='utf-8') as f:
+        rocks = json.load(f)
+
+    for rock in rocks:
+        print(rock['name'])
 
 
 def rock_list(request):
